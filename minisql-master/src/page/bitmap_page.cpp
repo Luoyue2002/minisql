e6e076e721090a1c10537c591001315e004cb6ec
luoyue <twlk2002@126.com>
2022-05-01 23:22:57 +0800

    Changes bitmap_page.cpp

--- minisql-master/src/page/bitmap_page.cpp
+++ minisql-master/src/page/bitmap_page.cpp
@@ -2,22 +2,55 @@
 
 template<size_t PageSize>
 bool BitmapPage<PageSize>::AllocatePage(uint32_t &page_offset) {
-  return false;
+    uint32_t *offset;
+    offset = &page_offset;
+    uint32_t byte_index = *offset / 8;
+    uint8_t bit_index = *offset % 8;
+    if ( IsPageFree(*offset) ){
+        bytes[byte_index] = bytes[byte_index] | bytebit[bit_index & 7];
+        page_allocated_ = page_allocated_ + 1;
+        uint32_t i;
+        for( i=0; i< GetMaxSupportedSize() ; i++){
+            if(IsPageFree(i)){
+                next_free_page_ = i;
+                break;
+            }
+        }
+        return true;
+    }
+    else
+        return false;
 }
 
 template<size_t PageSize>
 bool BitmapPage<PageSize>::DeAllocatePage(uint32_t page_offset) {
-  return false;
+    uint32_t byte_index = page_offset / 8;
+    uint8_t bit_index = page_offset % 8;
+    if ( !IsPageFree(page_offset) ){
+        bytes[byte_index] = bytes[byte_index] | bytebit[bit_index & 7];
+        page_allocated_ = page_allocated_ - 1;
+        if ( page_offset < next_free_page_)
+            next_free_page_ = page_offset;
+        return true;
+    }
+    else
+        return false;
 }
 
 template<size_t PageSize>
 bool BitmapPage<PageSize>::IsPageFree(uint32_t page_offset) const {
-  return false;
+    uint32_t byte_index = page_offset / 8;
+    uint8_t bit_index = page_offset % 8;
+    bool isfree = IsPageFreeLow( byte_index , bit_index);
+    return isfree;
 }
 
 template<size_t PageSize>
 bool BitmapPage<PageSize>::IsPageFreeLow(uint32_t byte_index, uint8_t bit_index) const {
-  return false;
+    if(bytes[byte_index] & bytebit[bit_index & 7]) // this bit is 1
+        return false;
+    else
+        return true;
 }
 
 template
