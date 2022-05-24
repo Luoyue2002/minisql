ca39a0cf42df21a7ad3b61289ad70dbea06b40bd
AwfAwe <929016211@qq.com>
2022-05-24 19:05:39 +0800

    CatalogmanagerNotDone

--- minisql-master/src/catalog/catalog.cpp
+++ minisql-master/src/catalog/catalog.cpp
@@ -1,20 +1,92 @@
 #include "catalog/catalog.h"
 
+/*
+  | MAGIC_NUM | 
+  | table_meta_pages_: n(size) + n*(table_id, page_id)
+  | index_meta_pages_: n(size) + n*(index_id, page_id)
+
+ */
 void CatalogMeta::SerializeTo(char *buf) const {
-  // ASSERT(false, "Not Implemented yet");
+  //MAGIC_NUM  
+  MACH_WRITE_TO(uint32_t, buf, CATALOG_METADATA_MAGIC_NUM); 
+  buf += Type::GetTypeSize(TypeId::kTypeInt);
+  // table_meta_pages_
+  MACH_WRITE_TO(uint32_t, buf, table_meta_pages_.size()); 
+  buf += Type::GetTypeSize(TypeId::kTypeInt);
+  auto it = table_meta_pages_.begin();
+  for(;it!=table_meta_pages_.end();it++){
+    MACH_WRITE_TO(uint32_t, buf, it->first); 
+    buf += Type::GetTypeSize(TypeId::kTypeInt);
+    MACH_WRITE_TO(uint32_t, buf, it->second); 
+    buf += Type::GetTypeSize(TypeId::kTypeInt);
+  }
+  // index_meta_pages_
+  MACH_WRITE_TO(uint32_t, buf, index_meta_pages_.size());
+  buf += Type::GetTypeSize(TypeId::kTypeInt);
+  for(it = index_meta_pages_.begin(); it != index_meta_pages_.end(); it++){
+    MACH_WRITE_TO(uint32_t, buf, it->first); 
+    buf += Type::GetTypeSize(TypeId::kTypeInt);
+    MACH_WRITE_TO(uint32_t, buf, it->second); 
+    buf += Type::GetTypeSize(TypeId::kTypeInt);
+  }
+
 }
 
 CatalogMeta *CatalogMeta::DeserializeFrom(char *buf, MemHeap *heap) {
-  // ASSERT(false, "Not Implemented yet");
-  return nullptr;
+  uint32_t MAGIC_NUM;
+  MAGIC_NUM = MACH_READ_FROM(uint32_t, buf);
+  buf += Type::GetTypeSize(TypeId::kTypeInt);
+  if(MAGIC_NUM != CATALOG_METADATA_MAGIC_NUM) return 0;
+
+  uint32_t table_id_=0;
+  uint32_t index_id_=0;
+  uint32_t page_id_ =0;
+  uint32_t Size = 0;
+  std::map<table_id_t, page_id_t> table_meta_pages_;
+  std::map<index_id_t, page_id_t> index_meta_pages_;
+  //index_meta_page
+  Size = MACH_READ_FROM(uint32_t, buf);
+  buf += Type::GetTypeSize(TypeId::kTypeInt);
+
+  for(uint32_t i=0; i<Size; ++i){
+    table_id_ = MACH_READ_FROM(uint32_t, buf);
+    buf += Type::GetTypeSize(TypeId::kTypeInt);
+    page_id_ = MACH_READ_FROM(int32_t, buf);
+    buf += Type::GetTypeSize(TypeId::kTypeInt);
+    table_meta_pages_[table_id_] = page_id_;
+  }
+  // index_meta_page
+  Size = MACH_READ_FROM(uint32_t, buf);
+  buf += Type::GetTypeSize(TypeId::kTypeInt);
+  for(uint32_t i=0; i<Size; ++i){
+    index_id_ = MACH_READ_FROM(uint32_t, buf);
+    buf += Type::GetTypeSize(TypeId::kTypeInt);
+    page_id_ = MACH_READ_FROM(int32_t, buf);
+    buf += Type::GetTypeSize(TypeId::kTypeInt);
+    index_meta_pages_[index_id_] = page_id_;
+  }
+  CatalogMeta *CatalogMeta_ = nullptr;
+  CatalogMeta_ = reinterpret_cast<CatalogMeta*>(heap->Allocate(sizeof(CatalogMeta)));
+  if(CatalogMeta_ == nullptr) return 0;
+
+  new(CatalogMeta_) CatalogMeta(table_meta_pages_,index_meta_pages_);
+
+  return CatalogMeta_;
 }
 
 uint32_t CatalogMeta::GetSerializedSize() const {
-  // ASSERT(false, "Not Implemented yet");
-  return 0;
+  uint32_t size = 0;
+  size += sizeof(uint32_t); // MAGIC_NUM
+  size += sizeof(uint32_t); // table_meta_pages_ size  
+  size += sizeof(uint32_t); // index_meta_pages_ size
+  size += sizeof(uint32_t) * table_meta_pages_.size() + sizeof(int32_t) * table_meta_pages_.size();
+  size += sizeof(uint32_t) * index_meta_pages_.size() + sizeof(int32_t) * index_meta_pages_.size();
+  return size;
 }
 
 CatalogMeta::CatalogMeta() {}
+CatalogMeta::CatalogMeta(std::map<table_id_t, page_id_t>tablepage_,std::map<index_id_t, page_id_t>indexpage_)
+    :table_meta_pages_(tablepage_),index_meta_pages_(indexpage_) {}
 
 
 CatalogManager::CatalogManager(BufferPoolManager *buffer_pool_manager, LockManager *lock_manager,
--- minisql-master/src/catalog/table.cpp
+++ minisql-master/src/catalog/table.cpp
@@ -1,18 +1,91 @@
 #include "catalog/table.h"
 
+/*
+  store the tablemetadata in the format: 
+  | TABLE_MD_MAGIC_NUM int | table_id_ int 
+  | table_name_(len int + name char*)
+  | root_page_id_ int | schema_
+*/
+
+
 uint32_t TableMetadata::SerializeTo(char *buf) const {
-  return 0;
+  
+  //MAGIC_NUM
+  MACH_WRITE_TO(uint32_t, buf, TABLE_METADATA_MAGIC_NUM); 
+  buf += Type::GetTypeSize(TypeId::kTypeInt);
+  
+  // table_id_
+  MACH_WRITE_TO(uint32_t, buf, table_id_); 
+  buf += Type::GetTypeSize(TypeId::kTypeInt);
+  
+  //table_name_len + table_name
+  MACH_WRITE_TO(uint32_t, buf, table_name_.size()); 
+  buf += Type::GetTypeSize(TypeId::kTypeInt);
+  std::memmove(buf, table_name_.data(), table_name_.size());
+  buf += table_name_.size()*sizeof(char);
+
+  //root_page_id_
+  MACH_WRITE_TO(uint32_t, buf, root_page_id_); 
+  buf += Type::GetTypeSize(TypeId::kTypeInt);
+
+  //schema serialize in schema.cpp  
+  buf += schema_->SerializeTo(buf);
+  
+  return GetSerializedSize();
 }
 
 uint32_t TableMetadata::GetSerializedSize() const {
-  return 0;
+  uint32_t size=0;
+  size += sizeof(uint32_t); // MAGIC_NUM
+  size += sizeof(uint32_t); // table_id_
+  size += sizeof(uint32_t); // index_name len
+  size += table_name_.size() * sizeof(char); // name char*
+  size += sizeof(uint32_t); // root_page_id
+  return size;
 }
 
 /**
  * @param heap Memory heap passed by TableInfo
  */
 uint32_t TableMetadata::DeserializeFrom(char *buf, TableMetadata *&table_meta, MemHeap *heap) {
-  return 0;
+  if (table_meta != nullptr) return 0;
+  
+  uint32_t MAGIC_NUM;
+  MAGIC_NUM = MACH_READ_FROM(u_int32_t, buf);
+  buf += Type::GetTypeSize(TypeId::kTypeInt);
+  if(MAGIC_NUM != TABLE_METADATA_MAGIC_NUM) return 0;
+  
+  table_id_t table_id_ = 0;
+  uint32_t name_len=0;
+  std::string table_name_="";
+  page_id_t root_page_id_ = 0;
+
+  // table_id_
+  table_id_ = MACH_READ_FROM(u_int32_t, buf);
+  buf += Type::GetTypeSize(TypeId::kTypeInt);
+
+  //table_name_len + char*
+  name_len = MACH_READ_FROM(u_int32_t, buf);
+  buf += Type::GetTypeSize(TypeId::kTypeInt);
+  table_name_.assign(buf, name_len);
+  buf += name_len *sizeof(char);
+
+  //root_page_id_
+  root_page_id_ = MACH_READ_FROM(u_int32_t, buf);
+  buf += Type::GetTypeSize(TypeId::kTypeInt);
+
+  //Schema
+  Schema *schema_ = NULL;
+  uint32_t cnt = Schema::DeserializeFrom(buf, schema_, heap);
+  buf += cnt;
+  table_meta = reinterpret_cast<TableMetadata*>(heap->Allocate(sizeof(TableMetadata)));
+  if(table_meta == nullptr){
+    // alloc failed
+    return 0;
+  }
+  new(table_meta) TableMetadata(table_id_, table_name_, root_page_id_, schema_);
+
+  return table_meta->GetSerializedSize();
 }
 
 /**
@@ -29,3 +102,4 @@ TableMetadata *TableMetadata::Create(table_id_t table_id, std::string table_name
 
 TableMetadata::TableMetadata(table_id_t table_id, std::string table_name, page_id_t root_page_id, TableSchema *schema)
         : table_id_(table_id), table_name_(table_name), root_page_id_(root_page_id), schema_(schema) {}
+
