a1027dab661f3f3717ec38e8089f3022565f3a4f
Luoyue2002 <104545011+Luoyue2002@users.noreply.github.com>
2022-05-24 17:09:03 +0800

    another insert table heap

--- minisql-master/src/storage/table_heap.cpp
+++ minisql-master/src/storage/table_heap.cpp
@@ -1,33 +1,75 @@
 #include "storage/table_heap.h"
 
 bool TableHeap::InsertTuple(Row &row, Transaction *txn) {
-  TablePage* page;
-  page_id_t page_id = first_page_id_;
-  bool re;
-  while( (page = reinterpret_cast<TablePage *>(buffer_pool_manager_->FetchPage(page_id)) ) != nullptr ) {
-    // how to make sure that whether the page can hold the row
-    /// need to find whether hold by the return value of TablePage::InsertTuple
-    page->WLatch();
-    re = page->InsertTuple(row, schema_, txn, lock_manager_, log_manager_);
-    page->WUnlatch();
-    buffer_pool_manager_->UnpinPage(page->GetTablePageId(), true);
-    if(re) return true;
-    page_id = page->GetNextPageId();
-    if(page_id == INVALID_PAGE_ID){
-      break;
+//  TablePage* page;
+//  page_id_t page_id = first_page_id_;
+//  bool re;
+//  while( (page = reinterpret_cast<TablePage *>(buffer_pool_manager_->FetchPage(page_id)) ) != nullptr ) {
+//    // how to make sure that whether the page can hold the row
+//    /// need to find whether hold by the return value of TablePage::InsertTuple
+//    page->WLatch();
+//    re = page->InsertTuple(row, schema_, txn, lock_manager_, log_manager_);
+//    page->WUnlatch();
+//    buffer_pool_manager_->UnpinPage(page->GetTablePageId(), true);
+//    if(re) return true;
+//    page_id = page->GetNextPageId();
+//    if(page_id == INVALID_PAGE_ID){
+//      break;
+//    }
+//  }
+//
+//  // if(page == nullptr) {
+//  /// the page must be nullptr to get out of the loop
+//  page = reinterpret_cast<TablePage *>(buffer_pool_manager_->NewPage(page_id) );
+//  if (!page) return false;
+//
+//  // how to insert data to the page
+//  /// TablePage::InsertTuple
+//  re = page->InsertTuple(row, schema_, txn, lock_manager_, log_manager_);
+//
+//  return re;
+    Page * page_now = buffer_pool_manager_->FetchPage(first_page_id_);
+    TablePage * table_page_now = reinterpret_cast<TablePage *>(page_now);
+    if(table_page_now != nullptr){
+      bool  finish = table_page_now->InsertTuple(row, schema_, txn, lock_manager_, log_manager_);
+      while (!finish) {
+        page_id_t next_page_id = table_page_now->GetNextPageId();
+
+        if (next_page_id != INVALID_PAGE_ID) {
+
+          buffer_pool_manager_->UnpinPage(table_page_now->GetTablePageId(), false);
+          table_page_now = static_cast<TablePage *>(buffer_pool_manager_->FetchPage(next_page_id));
+
+        } else {
+
+          TablePage * new_table_page = static_cast<TablePage *>(buffer_pool_manager_->NewPage( next_page_id));
+          if (new_table_page == nullptr) {
+
+            buffer_pool_manager_->UnpinPage(table_page_now->GetTablePageId(), false);
+            return false;
+          }
+
+          table_page_now->SetNextPageId(next_page_id);
+          table_page_now->Init(next_page_id,table_page_now->GetTablePageId(), log_manager_, txn);
+          table_page_now->WUnlatch();
+          buffer_pool_manager_->UnpinPage(table_page_now->GetTablePageId(), true);
+          table_page_now = new_table_page;
+          finish = table_page_now->InsertTuple(row, schema_, txn, lock_manager_, log_manager_);
+        }
+      }
+
+
+      buffer_pool_manager_->UnpinPage(table_page_now->GetTablePageId(), true);
+
+      return true;
     }
-  }
 
-  // if(page == nullptr) {
-  /// the page must be nullptr to get out of the loop
-  page = reinterpret_cast<TablePage *>(buffer_pool_manager_->NewPage(page_id) );
-  if (!page) return false;
 
-  // how to insert data to the page
-  /// TablePage::InsertTuple
-  re = page->InsertTuple(row, schema_, txn, lock_manager_, log_manager_);
+    else{
+      return false ;
+    }
 
-  return re;
+    return false;
 }
 
 bool TableHeap::MarkDelete(const RowId &rid, Transaction *txn) {
